78b9d921f54378434ded2ef5561d2a1a10753bdb
EvenW <yuhang0229@gmail.com>
2021-09-20 11:51:41 +1000

    create basic shopPage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
 
 import Homepage from './pages/Homepage/Homepage';
+import ShopPage from './pages/Shop/Shop';
+
+
 
 function App() {
   return (
     <div className="App">
-      <Homepage />
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
     </div>
   );
 }
