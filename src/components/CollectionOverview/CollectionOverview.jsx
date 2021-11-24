9be182a73a8f71fdcf191ae455f2a52ed9fdf796
Johnson <yuhang0229@gmail.com>
2021-11-24 16:31:06 +1100

    moved directory and shop data into its own reducer, created respetive selectors and updated correct data flow

---
+++ src/components/CollectionOverview/CollectionOverview.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+import { selectCollections } from "../../redux/shop/shop.selector";
+import CollectionPreview from "../CollectionPreview/CollectionPreview";
+import './collectionOverview.scss';
+
+const CollectionOverview = ({ collections }) => (
+  <div className='collections-overview'>
+    {
+      collections.map(({ id, ...otherCollectionProps }) => (
+        <CollectionPreview key={id} {...otherCollectionProps} />
+      ))
+    }
+  </div>
+);
+
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollections,
+});
+
+export default connect (mapStateToProps)(CollectionOverview);
--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -1,30 +1,14 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import CollectionOverview from '../../components/CollectionOverview/CollectionOverview';
+import CollectionPage from '../Collection/Collection';
 
-import CollectionPreview from '../../components/CollectionPreview/CollectionPreview';
-import SHOP_DATA from './shop.data';
+const ShopPage = ({ match }) => (
+  <div className='shop-page'>
+    <Route exact path={`${match.path}`} component={CollectionOverview} />
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+  </div>
+);
 
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      collections: SHOP_DATA
-    };
-  }
-
-  render() {
-    const { collections } = this.state;
-
-    return (
-      <div>
-        {
-          collections.map(({ id, ...otherCollectionProps }) => (
-            <CollectionPreview key={id} {...otherCollectionProps} />
-          ))
-        }
-      </div>
-    );
-  }
-}
 
 export default ShopPage;
